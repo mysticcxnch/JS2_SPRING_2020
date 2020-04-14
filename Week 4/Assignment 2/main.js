0148f28c9b16b74b258530ec3d948acbe1659c10
mysticcxnch <55148480+mysticcxnch@users.noreply.github.com>
2020-04-14 12:51:12 -0400

    additions

---
+++ Week 4/Assignment 2/main.js
@@ -0,0 +1,26 @@
+// ASSIGNMENT:  write the document ready function
+// PURPOSE OF CODE:  
+ 
+
+
+
+
+// ASSIGNMENT:  write the jQ code that shows the dropdown menu for the cart
+// PURPOSE OF CODE:  
+
+
+
+
+
+// ASSIGNMENT write the jQ code that shows the dropdown menu for the account
+// PURPOSE OF CODE:
+
+
+
+// ASSIGNMENT:  write the jQ code that shows the dropdown menu for help
+// PURPOSE OF CODE:
+
+
+
+// ASSIGNMENT:  write the jQ code that causes the menu to disappear
+// PURPOSE OF CODE:
