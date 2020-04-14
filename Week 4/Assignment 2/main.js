62616d138f6b2eed59ac0ad723ca4c8ebcc8bda8
mysticcxnch <55148480+mysticcxnch@users.noreply.github.com>
2020-04-14 13:41:26 -0400

    cha cha changes

--- Week 4/Assignment 2/main.js
+++ Week 4/Assignment 2/main.js
@@ -1,26 +1,41 @@
 // ASSIGNMENT:  write the document ready function
 // PURPOSE OF CODE:  
  
-
-
+$(document).ready(() =>
+        {
 
 
 // ASSIGNMENT:  write the jQ code that shows the dropdown menu for the cart
 // PURPOSE OF CODE:  
 
-
+            $('#cart').on('click', () =>
+                {
+                    $('#cartMenu').show();
+                })
 
 
 
 // ASSIGNMENT write the jQ code that shows the dropdown menu for the account
 // PURPOSE OF CODE:
 
-
+            $('account').on('click', () =>
+                {
+                    $('#accountMenu').show();
+                })
 
 // ASSIGNMENT:  write the jQ code that shows the dropdown menu for help
 // PURPOSE OF CODE:
 
-
+            $('#help').on('click', () =>
+                {
+                    $('#helpMenu').show();
+                })
 
 // ASSIGNMENT:  write the jQ code that causes the menu to disappear
 // PURPOSE OF CODE:
+
+            $('.dropdown-menu').on('mouseleave', () =>
+                {
+                    $('.dropdown-menu').hide();
+                })
+        })
